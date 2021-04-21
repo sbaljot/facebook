22be3e7e5a1b754c21988e6fb9375c67f27c7f52
sbaljot <54472640+sbaljot@users.noreply.github.com>
2021-04-21 11:53:10 +0530

    Update data.php

--- config/data.php
+++ config/data.php
@@ -1,1 +1,11 @@
-
+<?php
+    $conn = mysqli_connect("localhost","root","","facebook");
+    $query = "SELECT * FROM posts";
+    $query_run = mysqli_query($conn,$query);
+    $result_array = [];
+    foreach($query_run as $row){
+        array_push($result_array, $row);
+    }
+    header("Content-type: application/json");
+    echo json_encode($result_array);
+?>
